ac90e170a6b4ec40aa2d847eae5313171bcce34f
Annemarie Allen <annemarie@Annemaries-MacBook-Pro.local>
2020-04-16 12:42:10 -0800

    to create pull request for retro

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -25,6 +25,7 @@
 */
 
 const followersArray = [];
+a 
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
